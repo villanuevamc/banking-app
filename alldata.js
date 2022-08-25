3709d0c03bda7f791d111d672e089526c053a00d
Mariah Villanueva <mariah.c.villanueva@gmail.com>
2022-08-24 22:16:57 -0400

    Fix all data table headings

--- alldata.js
+++ alldata.js
@@ -9,8 +9,8 @@ function AllData() {
         <table className="table table-striped">
           <thead>
             <tr>
-              <th scope="col">Email</th>
               <th scope="col">Name</th>
+              <th scope="col">Email</th>
               <th scope="col">Password</th>
               <th scope="col">Balance</th>
             </tr>
